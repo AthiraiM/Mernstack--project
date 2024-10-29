4dfc24a0263243ee32314ae57e10a69c588300b7
SANTHOSPARIMOHAN <143578029+SANTHOSPARIMOHAN@users.noreply.github.com>
2024-10-29 09:44:41 +0530

    Create App.js

---
+++ client/cilent/App.js
@@ -0,0 +1,25 @@
+// client/src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Todo from "./components/Todo";
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todo" element={<PrivateRoute><Todo /></PrivateRoute>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
